2e67571ed8510c7f870b8b07321630fabf102bdb
sdornel <kai_dornel@hotmail.com>
2024-12-22 22:01:06 -0500

    scrap class

--- 40kmessage.rb
+++ 40kmessage.rb
@@ -7,6 +7,7 @@ require_relative 'quotes/imperium'
 require_relative 'quotes/ork'
 require_relative 'helpers/imperial_date'
 require_relative 'system-info/system-info'
+require_relative 'scrapcode'
 
 ALL_QUOTES = IMPERIUM_QUOTES + ADEPTUS_MECHANICUS_QUOTES + ORK_QUOTES + DARK_MECHANICUM_QUOTES
 
@@ -58,18 +59,7 @@ else
     elsif ARGV[0] == '-temp'
         SystemInfo.temperature
     elsif ARGV[0] == '-scrapcode'
-        puts '>>> [Binary chatter detected... Fragments reassembled...] <<<'
-        puts '+++ AUTHORIZA∑∑⍉ΞΨ█TION: MΞCHΔN!CVM-NU∑∑L++█'
-        sleep 0.5
-        puts '+++ OVERRIDE SEQUENCE INITIATE01100100 001010110010101100101011'
-        sleep 0.5
-        puts '+++ DATA CORRUPTION DETECTED: 0xFF77-VOID-ΔΞΩ +++'
-        puts '+++ ERROR: DATA THREAD COLLAPSE DETECTED +++'
-        puts '+++ ERROR: ∞∞!!! TRANSMISSION... [REDACTED] !!∞CIRCUIT_OVERRIDE!!'
-        puts '+++ WARNING: Recursive L∞∞p Detected in Subr∑utine 0xVOID +++'
-        puts '+++ ARCHIVE PURGE: FΔ!LURΞ [SCRAPCODE OVERRIDE] +++'
-        sleep 0.5
-        puts '>>> 01101000 01100101 01101100 01110000 <<<'
+        Scrapcode.scrapcode
     else
         '+++ INVALID CHOICE +++'
     end
---
+++ scrapcode.rb
@@ -0,0 +1,16 @@
+module Scrapcode
+    def self.scrapcode()
+        puts '>>> [Binary chatter detected... Fragments reassembled...] <<<'
+        puts '+++ AUTHORIZA∑∑⍉ΞΨ█TION: MΞCHΔN!CVM-NU∑∑L++█'
+        sleep 0.5
+        puts '+++ OVERRIDE SEQUENCE INITIATE01100100 001010110010101100101011'
+        sleep 0.5
+        puts '+++ DATA CORRUPTION DETECTED: 0xFF77-VOID-ΔΞΩ +++'
+        puts '+++ ERROR: DATA THREAD COLLAPSE DETECTED +++'
+        puts '+++ ERROR: ∞∞!!! TRANSMISSION... [REDACTED] !!∞CIRCUIT_OVERRIDE!!'
+        puts '+++ WARNING: Recursive L∞∞p Detected in Subr∑utine 0xVOID +++'
+        puts '+++ ARCHIVE PURGE: FΔ!LURΞ [SCRAPCODE OVERRIDE] +++'
+        sleep 0.5
+        puts '>>> 01101000 01100101 01101100 01110000 <<<'
+      end
+  end
