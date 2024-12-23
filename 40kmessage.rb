95b1c18d8478f2bad233cb389c9e1ea713c261e7
sdornel <kai_dornel@hotmail.com>
2024-12-22 21:08:30 -0500

    scrapcode stuff

--- 40kmessage.rb
+++ 40kmessage.rb
@@ -56,8 +56,21 @@ else
         SystemInfo.battery_level
     elsif ARGV[0] == '-temp'
         SystemInfo.temperature
+    else # make it an override option?
+        puts '>>> [Binary chatter detected... Fragments reassembled...] <<<'
+        puts '+++ AUTHORIZA∑∑⍉ΞΨ█TION: MΞCHΔN!CVM-NU∑∑L++█'
+        sleep 0.5
+        puts '+++ OVERRIDE SEQUENCE INITIATE01100100 001010110010101100101011'
+        sleep 0.5
+        puts '+++ DATA CORRUPTION DETECTED: 0xFF77-VOID-ΔΞΩ +++'
+        puts '+++ ERROR: DATA THREAD COLLAPSE DETECTED +++'
+        puts '+++ ERROR: ∞∞!!! TRANSMISSION... [REDACTED] !!∞CIRCUIT_OVERRIDE!!'
+        puts '+++ WARNING: Recursive L∞∞p Detected in Subr∑utine 0xVOID +++'
+        puts '+++ ARCHIVE PURGE: FΔ!LURΞ [SCRAPCODE OVERRIDE] +++'
+        sleep 0.5
+        puts '>>> 01101000 01100101 01101100 01110000 <<<'
     end
-
+    puts ''
     puts "---------------------------------------------------------------------"
     puts ''
     puts "+++ THOUGHT FOR THE DAY: #{ALL_QUOTES.sample} +++"
