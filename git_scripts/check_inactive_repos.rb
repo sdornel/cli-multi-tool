3c7cb689fe98ae0a4465a5208b572fb2bed58cd0
sdornel <kai_dornel@hotmail.com>
2024-12-28 17:17:59 -0500

    explain function

--- git_scripts/check_inactive_repos.rb
+++ git_scripts/check_inactive_repos.rb
@@ -29,6 +29,7 @@ module CheckInactiveRepos
         puts self.fetch_specific_inactive_repos
     end
 
+    # displays inactive repos (>= 365 days without updates from specific list)
     def self.fetch_specific_inactive_repos()
         target_repos = {
             '1904-text-searcher' => true,
