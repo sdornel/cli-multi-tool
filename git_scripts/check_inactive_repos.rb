4137339b90abe0577f32451b9aeaa3b1d0e5e499
sdornel <kai_dornel@hotmail.com>
2024-12-28 01:57:53 -0500

    connect to github via octotkit

--- git_scripts/check_inactive_repos.rb
+++ git_scripts/check_inactive_repos.rb
@@ -1,13 +1,32 @@
+
+
 require 'octokit'
 require 'date'
 require 'dotenv'
 
 Dotenv.load
 
-octokit = Octokit::Client.new(access_token: "YOUR-TOKEN")
+GITHUB_TOKEN = ENV['GITHUB_TOKEN']
+GITHUB_USERNAME = ENV['GITHUB_USERNAME']
+
+if GITHUB_TOKEN.nil? || GITHUB_TOKEN.empty?
+    puts "ERROR: GITHUB_TOKEN cannot be empty! Is the token set in your .env file?"
+    exit()
+end
 
 module CheckInactiveRepos
+    @client = Octokit::Client.new(access_token: GITHUB_TOKEN)
     def self.fetch_repositories
-        puts "hi2"
+        puts "CONNECTING TO GITHUB API"
+        all_repos = []
+        # begin
+        puts @client.user
+        puts "+++ CONNECTING TO THE GITHUB +++"
+        @client.auto_paginate = true
+        repos = @client
+        # binding.pry
+        # rescue
+        #     puts "oops"
+        # end
     end
 end
