1d98e3a0e9de44cd7519f88ae9d344b07ecd6944
sdornel <kai_dornel@hotmail.com>
2024-12-29 20:04:23 -0500

    todo and cleanup

--- git_scripts/pull_request.rb
+++ git_scripts/pull_request.rb
@@ -1,10 +1,4 @@
 require 'octokit'
-# require 'dotenv'
-
-# Dotenv.load
-
-# GITHUB_TOKEN = ENV['GITHUB_TOKEN']
-# GITHUB_USERNAME = ENV['GITHUB_USERNAME']
 
 if GITHUB_TOKEN.nil? || GITHUB_TOKEN.empty?
     puts "ERROR: GITHUB_TOKEN cannot be empty! Is the token set in your .env file?"
@@ -15,6 +9,7 @@ module PullRequest
     @client = Octokit::Client.new(access_token: GITHUB_TOKEN)
     @client.auto_paginate = true
     
+    # TODO: add flag that checks for newer PRs
     def self.get_open_prs()
         new_prs = []
 
