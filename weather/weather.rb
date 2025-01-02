40bf223c48176a7f54ca618105c5f56088f522c7
sdornel <kai_dornel@hotmail.com>
2025-01-01 23:04:12 -0500

    formatting adjustments

--- weather/weather.rb
+++ weather/weather.rb
@@ -13,31 +13,19 @@ module Weather
 
     def self.get_weather_forecast
         location = get_location_from_ip
-        puts "Latitude: #{location[:lat]}, Longitude: #{location[:lon]}"
-        uri = URI("https://api.open-meteo.com/v1/forecast?latitude=#{location[:lat]}&longitude=#{location[:lon]}&current=apparent_temperature&hourly=temperature_2m,apparent_temperature,visibility,wind_speed_10m&timezone=auto&daily=")
+        uri = URI("https://api.open-meteo.com/v1/forecast?latitude=#{location[:lat]}&longitude=#{location[:lon]}&current=temperature_2m,apparent_temperature&hourly=temperature_2m,apparent_temperature,visibility,wind_speed_10m&timezone=auto&daily=")
         res = Net::HTTP.get_response(uri)
-        puts res.body if res.is_a?(Net::HTTPSuccess)
         data = JSON.parse(res.body, symbolize_names: true)
-        puts "Current Time: #{Time.now} (#{Time.now.zone})"
-        puts "Elevation: #{data[:elevation]}"
-        puts "Current Temperature: #{data[:current][:apparent_temperature]}#{data[:current_units][:apparent_temperature]}"
-        puts "Forecast for the days: "
-        
-        data[:daily][:time].each_with_index do |time, index|
-            if index == data[:daily][:time].last
-                print "#{time}"
-            else
-                print "#{time}, "
-            end
-        end
+
+        puts "7 Day Weather Forecast Report"
         data[:hourly][:time].each_with_index do |time, index|
             formatted_time = Time.parse(time).strftime('%b %d, %H:%M')
             if data[:hourly][:visibility][index] <= 1000
                 puts "| 🕒 #{formatted_time} " \
                         "| 🌡️ Temp: #{data[:hourly][:temperature_2m][index]}°C " \
                         "| 🥶 Feels Like: #{data[:hourly][:apparent_temperature][index]}°C " \
-                        "| 👀 Visibility: #{data[:hourly][:visibility][index]}m " \
-                        "| 💨 Wind: #{data[:hourly][:wind_speed_10m][index]} km/h |"
+                        "| 💨 Wind: #{data[:hourly][:wind_speed_10m][index]} km/h" \
+                        "| 👀 Visibility: #{data[:hourly][:visibility][index]}m |"
             else
                 puts "| 🕒 #{formatted_time} " \
                         "| 🌡️ Temp: #{data[:hourly][:temperature_2m][index]}°C " \
@@ -45,5 +33,11 @@ module Weather
                         "| 💨 Wind: #{data[:hourly][:wind_speed_10m][index]} km/h |"
             end
         end
+        puts ''
+        puts "Latitude: #{location[:lat]}, Longitude: #{location[:lon]}"
+        puts "Current Time: #{Time.now} (#{Time.now.zone})"
+        puts "Elevation: #{data[:elevation]}"
+        puts "Current Temperature: #{data[:current][:temperature_2m]}#{data[:current_units][:apparent_temperature]}"
+        puts "Feels Like: #{data[:current][:apparent_temperature]}#{data[:current_units][:apparent_temperature]}"
     end
 end
