6ac849bf3b6593924218803090e217c4d1354825
sdornel <kai_dornel@hotmail.com>
2025-01-04 22:26:39 -0500

    add colorable string

--- system_info/system_info.rb
+++ system_info/system_info.rb
@@ -1,31 +1,30 @@
+require_relative '../helpers/colorable_string/colorable_string'
+using ColorableString
+
 module SystemInfo
     def self.battery_level
-        command = "upower -i $(upower -e)"
+        command = 'upower -i $(upower -e)'
         output = `#{command}`
     
         if output.empty?
-        puts '>>> Power information not available.'
-        return nil
+          puts 'Power information not available.'.fg_color(:red)
         else
-            puts '>>> Power Status Report:'
-            puts '>>> via - upower -i $(upower -e)'
-            puts "#{output}"
-            return output
+            puts 'Power Status Report:'.fg_color(:cyan)
+            puts 'via - upower -i $(upower -e)'.fg_color(:yellow)
+            puts output.fg_color(:green)
         end
     end
 
     def self.temperature
-        command = "sensors" # could be more efficient but this will help me learn the commands
+        command = 'sensors' # could be more efficient but this will help me learn the commands
         output = `#{command}`
     
         if output.empty?
-          puts ">>> Temperature information not available."
-          return nil
+          puts 'Temperature information not available.'.fg_color(:red)
         else
-          puts ">>> Temperature Report:"
-          puts ">>> via sensors"
-          puts output
-          return output
+          puts 'Temperature Report:'.fg_color(:cyan)
+          puts 'via sensors'.fg_color(:yellow)
+          puts output.fg_color(:green)
         end
     end
 end
