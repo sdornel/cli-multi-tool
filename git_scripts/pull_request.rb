dc68ba78210683e88f1227890223ec06bedbcf58
sdornel <kai_dornel@hotmail.com>
2025-01-05 00:04:05 -0500

    colorize

--- git_scripts/pull_request.rb
+++ git_scripts/pull_request.rb
@@ -1,7 +1,7 @@
 require 'octokit'
-
+using ColorableString
 if GITHUB_TOKEN.nil? || GITHUB_TOKEN.empty?
-    puts "ERROR: GITHUB_TOKEN cannot be empty! Is the token set in your .env file?"
+    puts 'ERROR: GITHUB_TOKEN cannot be empty! Is the token set in your .env file?'.fg_color(:red)
     exit()
 end
 
@@ -14,14 +14,25 @@ module PullRequest
         new_prs = []
 
         begin
-            puts "Fetching open pull requests for repository: #{repo_name}"
+            puts "\nFetching open pull requests for repository: #{repo_owner}/#{repo_name}"
             pull_requests = @client.pull_requests("#{repo_owner}/#{repo_name}", state: 'open')
-            pull_requests.each do |pr|
-                puts "##{pr[:number]} | #{pr[:title]} | #{pr[:user][:login]} | #{pr[:html_url]}"
+
+            if pull_requests.empty?
+                puts 'No open pull requests found.'
+                return
             end
+            pull_requests.each do |pr|
+                puts <<~INFO
+                    #{"##{pr[:number]}".fg_color(:cyan)} | \
+                    #{"#{pr[:title]}".fg_color(:green)} | \
+                    #{"#{pr[:user][:login]}".fg_color(:yellow)} | \
+                    #{"#{pr[:html_url]}".fg_color(:purple)}
+                INFO
+            end
+
         rescue Octokit::Error => error
             if error.response_body
-                puts "Error fetching repositories! Message: #{error.response_body}"
+                puts "Error fetching pull requests! Message: #{error.response_body}".fg_color(:red)
             end
         end
     end
