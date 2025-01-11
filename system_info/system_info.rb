0f345097120d3c1d1d0fab17f007cfbd71b286f4
sdornel <kai_dornel@hotmail.com>
2025-01-10 22:55:25 -0500

    fix indentation

--- system_info/system_info.rb
+++ system_info/system_info.rb
@@ -3,29 +3,29 @@ using ColorableString
 
 module SystemInfo
   extend self
-    def battery_level
-        command = 'upower -i $(upower -e)'
-        output = `#{command}`
-    
-        if output.empty?
-          puts 'Power information not available.'.fg_color(:red)
-        else
-            puts 'Power Status Report:'.fg_color(:cyan)
-            puts 'via - upower -i $(upower -e)'.fg_color(:yellow)
-            puts output.fg_color(:green)
-        end
-    end
+  def battery_level
+    command = 'upower -i $(upower -e)'
+    output = `#{command}`
 
-    def temperature
-        command = 'sensors' # could be more efficient but this will help me learn the commands
-        output = `#{command}`
-    
-        if output.empty?
-          puts 'Temperature information not available.'.fg_color(:red)
-        else
-          puts 'Temperature Report:'.fg_color(:cyan)
-          puts 'via sensors'.fg_color(:yellow)
-          puts output.fg_color(:green)
-        end
+    if output.empty?
+      puts 'Power information not available.'.fg_color(:red)
+    else
+        puts 'Power Status Report:'.fg_color(:cyan)
+        puts 'via - upower -i $(upower -e)'.fg_color(:yellow)
+        puts output.fg_color(:green)
     end
+  end
+
+  def temperature
+    command = 'sensors' # could be more efficient but this will help me learn the commands
+    output = `#{command}`
+
+    if output.empty?
+      puts 'Temperature information not available.'.fg_color(:red)
+    else
+      puts 'Temperature Report:'.fg_color(:cyan)
+      puts 'via sensors'.fg_color(:yellow)
+      puts output.fg_color(:green)
+    end
+  end
 end
