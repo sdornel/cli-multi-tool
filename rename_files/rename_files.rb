755fb796dc93ef12a9a416659b7172a50286bd4c
sdornel <kai_dornel@hotmail.com>
2025-01-12 18:44:55 -0500

    store renaming data

--- rename_files/rename_files.rb
+++ rename_files/rename_files.rb
@@ -1,9 +1,17 @@
 module RenameFile
     extend self
     def rename_files_with_sequential_naming_pattern
-        puts "Enter the directory. Default is current directory"
+        puts 'Enter the directory. Default is current directory.'
         directory = $stdin.gets.chomp
         directory = "." if directory.empty?
 
+        puts 'Enter file extension (.mp3, .odt, .txt) or leave blank.'
+        file_extension = $stdin.gets.chomp
+
+        puts 'Enter text you want replaced.'
+        to_replace = $stdin.gets.chomp
+
+        puts 'Enter replacement text'
+        replacing = $stdin.gets.chomp
     end
 end
