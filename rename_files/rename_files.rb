edf8dd917c7307b4965402b4f0cad64dd6b901fe
sdornel <kai_dornel@hotmail.com>
2025-01-13 21:42:28 -0500

    mass rename parts of files

--- rename_files/rename_files.rb
+++ rename_files/rename_files.rb
@@ -9,9 +9,22 @@ module RenameFile
         file_extension = $stdin.gets.chomp
 
         puts 'Enter text you want replaced.'
-        to_replace = $stdin.gets.chomp
+        old_text = $stdin.gets.chomp
 
         puts 'Enter replacement text'
-        replacing = $stdin.gets.chomp
+        new_text = $stdin.gets.chomp
+
+        puts "Searching with pattern: #{directory}/#{file_extension}"
+
+        Dir.glob("#{directory}/*#{file_extension}").each do |file_path|
+            dir_name = File.dirname(file_path)
+            base_name = File.basename(file_path)
+
+            # Replace the old_text with new_text in the filename
+            new_base_name = base_name.gsub(old_text, new_text)
+            new_file_path = File.join(dir_name, new_base_name)
+    
+            File.rename(file_path, new_file_path)
+        end
     end
 end
