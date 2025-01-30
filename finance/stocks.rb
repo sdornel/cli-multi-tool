fae4ff1e67d9b626981ffb0c2cd18199b44464bd
sdornel <kai_dornel@hotmail.com>
2025-01-30 14:01:20 +0800

    add extend self

--- finance/stocks.rb
+++ finance/stocks.rb
@@ -3,8 +3,9 @@ require 'json'
 using ColorableString
 
 module Stocks
+    extend self
     def from_list_retrieve_stock_data
-
+        # uri = URI.parse("https://")
     end
 
     def retrieve_specific_stock_data
