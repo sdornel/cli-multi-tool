914b7ff832ee50987042826b2336ddc9133516d7
sdornel <kai_dornel@hotmail.com>
2025-03-15 21:44:48 -0400

    declare empty functions in crypto file

---
+++ finance/crypto.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'json'
+using ColorableString
+
+module Crypto
+    def retrieve_specific_crypto_data
+
+    end
+
+    def from_list_retrieve_crypto_data
+
+    end
+end
