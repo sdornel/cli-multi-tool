1cf9927a6ad83df826635462d306280e3c458ba0
sdornel <kai_dornel@hotmail.com>
2025-03-15 21:58:26 -0400

    crypto hash

--- finance/crypto.rb
+++ finance/crypto.rb
@@ -3,6 +3,21 @@ require 'json'
 using ColorableString
 
 module Crypto
+    COINS = {
+        "bitcoin"        => "Bitcoin",
+        "ethereum"       => "Ethereum",
+        "tether"         => "Tether (USDT)",
+        "binancecoin"    => "BNB",
+        "solana"         => "Solana",
+        "xrp"            => "XRP",
+        "usd-coin"       => "USD Coin (USDC)",
+        "cardano"        => "Cardano",
+        "dogecoin"       => "Dogecoin",
+        "avalanche-2"    => "Avalanche",
+        "shiba-inu"      => "Shiba Inu",
+        "stellar"        => "Stellar (XLM)"
+    }
+
     def retrieve_specific_crypto_data
 
     end
